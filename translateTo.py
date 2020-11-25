529f41b65008e40686af995756bbb0ec9f336b06
Fawaz Ahmed <fawazahmed0@hotmail.com>
2020-11-25 20:30:06 +0530

    unclean working code 2 for trans to all langs

--- translateTo.py
+++ translateTo.py
@@ -1,43 +1,42 @@
 import sys
 import json
 import urllib.request
-# Refer https://github.com/ssut/py-googletrans to see examples on how to use this package
-from google_trans_new import google_translator  
-translator = google_translator()  
+# Refer https://github.com/lushan88a/google_trans_new to see examples on how to use this package
+from google_trans_new import google_translator
+import random
+import time
+translator = google_translator(url_suffix="hk",timeout=10)  
 
-
-# use Ubuntu 20.04.1 LTS in actions
-
-# Give a string to it as cmd argument and it will return all different languages & it's pronunciation
-
-#translator = Translator(service_urls=['translate.googleapis.com']) 
-#translator = Translator(service_urls=['translate.google.com']) 
-
-
-
-googlecodes = 'https://cdn.jsdelivr.net/gh/fawazahmed0/quran-api@1/isocodes/google-codes.min.json'
-req = urllib.request.Request(googlecodes)
-
-args = sys.argv
-
-# Removing the script name from args
-args.pop(0)
 sys.stdout.reconfigure(encoding='utf-8')
 
+# use Ubuntu 20.04.1 LTS in actions
+g_codes_link = 'https://cdn.jsdelivr.net/gh/fawazahmed0/quran-api@1/isocodes/google-codes.min.json'
+req = urllib.request.Request(g_codes_link)
 ##parsing response
-r = urllib.request.urlopen(req).read()
-cont = json.loads(r.decode('utf-8'))
+read_res = urllib.request.urlopen(req).read()
+g_codes = json.loads(read_res.decode('utf-8'))
 
-for key in cont.keys():
+
+args = sys.argv
+# Removing the script name from args
+args.pop(0)
+
+
+translated_list = []
+
+
+
+for key in g_codes.keys():
+#for key in ['hi','ar']:
     #translation = translator.translate('hamein kyun aisa nahi karna',dest=key)
+    time.sleep(random.uniform(0.2, 0.5))
     translate_text = translator.translate(args[0],lang_tgt=key)  
-    print(translate_text)
-    #print(key)
+    translated_list.append(translate_text)
     #print(translation.text)
     #print(translation.pronunciation)
 
 
-
+sys.stdout.write(json.dumps(translated_list))
 
 # https://stackoverflow.com/a/52372390
 # Not setting to utf-8 explicitly as python3 is utf-8 by default and it requires python 3.7+ to run the below command
